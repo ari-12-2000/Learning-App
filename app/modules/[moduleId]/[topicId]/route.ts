c9c19a8c09540e503a123673573de7e04a6a48eb
ari-12-2000 <arikum12000@gmail.com>
2025-06-20 18:18:27 +0530

    fixed routes

--- app/modules/[moduleId]/[topicId]/route.ts
+++ app/modules/[moduleId]/[topicId]/route.ts
@@ -1,6 +1,6 @@
-import { mapModulesAndTopics, ModuleController } from "@/controllers/moduleController"
+import { ModuleController } from "@/controllers/moduleController"
 import { NextRequest } from "next/server"
 
 export async function POST(req:NextRequest, {params}:{params:Promise<{moduleId:string, topicId:string}>}){
-    return mapModulesAndTopics(req,{params});
+    return ModuleController.mapModulesAndTopics(req,{params});
 }
